90c1e213b40c32996861a1b2cbf8eb35bcc741a9
Michal Čihař <michal@cihar.com>
2017-11-19 08:40:37 +0100

    Add install script
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ignore.d
+ls | while read name ; do
+    path=/etc/logcheck/ignore.d.server/nijel-$name
+    if [ ! -f $path ] ; then
+        ln -s `readlink -f $name` $path
+    fi
+done
