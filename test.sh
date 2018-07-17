ec4ffecf5c0c819cad41fbaa885f45a512d285a9
Michal Čihař <michal@cihar.com>
2018-07-17 09:22:30 +0200

    Verbose output for tests
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test.sh
+++ test.sh
@@ -3,12 +3,20 @@
 FAIL=0
 
 for file in test/* ; do
-    RESULT=$(logcheck-test -q -i -l $file -r ignore.d/`basename $file` | wc -l)
+    NAME=`basename $file`
+    echo -n "Testing $NAME ... "
+    RESULT=$(logcheck-test -q -i -l $file -r ignore.d/$NAME | wc -l)
     if [ $RESULT -ne 0 ] ; then
-        echo FAILED:
+        tput setaf 9
+        echo FAILED
+        tput sgr0
         logcheck-test -i -l $file -r ignore.d/`basename $file`
         echo
         FAIL=$(($FAIL + 1))
+    else
+        tput setaf 10
+        echo OK
+        tput sgr0
     fi
 done
 
